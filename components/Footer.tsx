fab9cf3f495d2d2896fdc483d653fd8490373a40
chanakyha <vetrichanakyha2003@gmail.com>
2024-01-28 16:15:46 +0530

    minor ui changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 const Footer = () => {
   return (
-    <div className="w-full border-t-2 p-1 flex justify-center bg-black">
+    <div className="w-full border-t-2 p-1 flex justify-center bg-background">
       <Button asChild className="text-xs" variant={"link"}>
         <Link href={"https://github.com/chanakyha"} target="_blank">
           Designed and Developed by Chan
